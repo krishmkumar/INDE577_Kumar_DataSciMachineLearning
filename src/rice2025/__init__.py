adb24c4ec5f4a691ead8e2b693e58435eab4da15
krishmkumar <145507048+krishmkumar@users.noreply.github.com>
2025-11-17 23:35:15 -0600

    Add root package init

--- src/rice2025/__init__.py
+++ src/rice2025/__init__.py
@@ -1,2 +1,26 @@
 __all__ = ["preprocess", "metrics", "knn", "postprocess"]
 
+"""
+rice2025 - A lightweight educational machine learning package.
+"""
+
+from .basic_functions import add
+from .metrics import euclidean_distance, manhattan_distance
+from .preprocess import normalize, train_test_split
+from .postprocess import majority_label, average_label
+
+# Expose subpackages
+from . import supervised_learning
+from . import unsupervised_learning
+
+__all__ = [
+    "add",
+    "euclidean_distance",
+    "manhattan_distance",
+    "normalize",
+    "train_test_split",
+    "majority_label",
+    "average_label",
+    "supervised_learning",
+    "unsupervised_learning",
+]
