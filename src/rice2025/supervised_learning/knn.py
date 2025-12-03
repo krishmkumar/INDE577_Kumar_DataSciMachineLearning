4d8772ad8de4549aa6aa7cf73a8700371cdb6fe0
krishmkumar <145507048+krishmkumar@users.noreply.github.com>
2025-12-02 21:21:12 -0600

    Fix KNNClassifier: inherit base class and support n_neighbors

--- src/rice2025/supervised_learning/knn.py
+++ src/rice2025/supervised_learning/knn.py
@@ -25,12 +25,27 @@ class _KNNBase:
 
 
 class KNNClassifier(_KNNBase):
+    """
+    KNN Classifier supporting both k= and n_neighbors=
+    """
+
+    def __init__(self, k=3, n_neighbors=None):
+        # Allow both styles: k= or n_neighbors=
+        if n_neighbors is not None:
+            super().__init__(n_neighbors=n_neighbors)
+        else:
+            super().__init__(n_neighbors=k)
+
     def predict(self, X):
         X = np.asarray(X, dtype=float)
-        return np.array([majority_label(self._get_neighbors(x)) for x in X])
+        return np.array([
+            majority_label(self._get_neighbors(x)) for x in X
+        ])
 
 
 class KNNRegressor(_KNNBase):
     def predict(self, X):
         X = np.asarray(X, dtype=float)
-        return np.array([average_label(self._get_neighbors(x)) for x in X])
+        return np.array([
+            average_label(self._get_neighbors(x)) for x in X
+        ])
